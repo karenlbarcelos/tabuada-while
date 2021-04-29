308b57cae01afe6edf3b334b62ddfc2fd9ef8534
Karen <karenlbarcelos@hotmail.com>
2021-04-29 17:26:00 -0300

    Codigo Tabuada

---
+++ ex20.py
@@ -0,0 +1,9 @@
+#Problema "tabuada" Ler um número inteiro N, daí mostrar na tela a tabuada de N para 1 a 10.
+
+numero = int(input("Digite um número: "))
+cont = 0
+
+while cont <= 10:
+	tabuada = numero * cont
+	print(numero ," x ", cont, " = ", tabuada)
+	cont +=1
